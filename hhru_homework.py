b6edfe7056f18f3e2f8a75b932603e8067f74406
you2zz <yri_vla@mail.ru>
2023-08-03 17:51:17 +0500

    03082023_1751

--- hhru_homework.py
+++ hhru_homework.py
@@ -4,7 +4,8 @@
 # <a class="serp-item__title" data-qa="serp-item__title" target="_blank" href="https://spb.hh.ru/vacancy/80002041?from=vacancy_search_list&amp;query=python"> - ссылка вакансии
 # <span data-qa="vacancy-serp__vacancy-compensation" class="bloko-header-section-2"> - зарплата вакансии
 # <div class="vacancy-serp-item__meta-info-company"> - компания
-#  <div data-qa="vacancy-serp__vacancy-address" class="bloko-text"> - город
+#  <div data-qa="vacancy-serp__vacancy-address" class="bloko-text"> -
+#  <div class="g-user-content"> -
 
 import json
 import requests
@@ -14,13 +15,25 @@ from urllib.parse import urljoin
 
 headers_gen = fake_headers.Headers(browser="firefox", os="win")
 
-response = requests.get("https://spb.hh.ru/search/vacancy?text=python&area=1&area=2",headers=headers_gen.generate())
+params = {
+    # 'search_field': 'name',
+    # 'search_field': 'company_name',
+    # 'search_field': 'description',
+    'enable_snippets': 'true',
+    'items_on_page':3,
+    'page':2
+}
+
+response = requests.get("https://spb.hh.ru/search/vacancy?text=python&area=1&area=2", params=params, headers=headers_gen.generate())
 html_data = response.text
 
 hh_filter = BeautifulSoup(html_data, 'lxml')
 vacancy_list_tag = hh_filter.find('div', id="a11y-main-content")
-# vacancy_tags = vacancy_list_tag.find_all('div', class_="serp-item")
-vacancy_tags = vacancy_list_tag.find_all('div', class_="vacancy-serp-item__layout")
+# if vacancy_list_tag != None:
+vacancy_tags = vacancy_list_tag.find_all('div', class_="serp-item")
+# else:
+
+
 
 vacancy_parsed = []
 for vacancy_tag in vacancy_tags:
@@ -29,11 +42,13 @@ for vacancy_tag in vacancy_tags:
     company_tag = vacancy_tag.find('div', class_="vacancy-serp-item__meta-info-company")
     city_tag = vacancy_tag.find('div', attrs = {'data-qa': 'vacancy-serp__vacancy-address'})
     salary_tag = vacancy_tag.find('span', attrs={"data-qa": "vacancy-serp__vacancy-compensation"})
+    description_tag = vacancy_tag.find('div', class_="g-user-content")
 
     header_text = header_tag.text
     link = a_tag['href']
     company_text = company_tag.text
     city_text = city_tag.text.split()[0].strip(',')
+    description_text = description_tag.text
 
     if salary_tag != None:
         salary_text = salary_tag.text
@@ -45,11 +60,9 @@ for vacancy_tag in vacancy_tags:
         'link': link,
         'salary': salary_text,
         'company': company_text,
-        'city': city_text
+        'city': city_text,
+        'description': description_text
     })
 
-print(vacancy_parsed)
-print(len(vacancy_parsed))
-
-# with open('vacancy_parsed2.json', 'a', encoding='utf-8') as file:
-#     json.dump(vacancy_parsed, file, indent=4, ensure_ascii=False)
+with open('vacancy_parsed.json', 'w', encoding='utf-8') as file:
+    json.dump(vacancy_parsed, file, indent=4, ensure_ascii=False)
