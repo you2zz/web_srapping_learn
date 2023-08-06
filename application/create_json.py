b64f99c0c196fd27272955d9c05d64e89b9caa06
You2zz <yri_vla@mail.ru>
2023-08-06 20:32:38 +0500

    06082023_2032

--- application/create_json.py
+++
@@ -1,6 +0,0 @@
-import json
-
-
-def create_json(data_in ):
-    with open('data/vacancy_parsed.json', 'w', encoding='utf-8') as file:
-        json.dump(data_in, file, indent=4, ensure_ascii=False)
