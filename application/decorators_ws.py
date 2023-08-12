a70dfc41f51bf96dd626a36d9d4bee1a7d2c4c8d
You2zz <yri_vla@mail.ru>
2023-08-12 23:07:41 +0500

    12082023_2307

--- application/decorators_ws.py
+++ application/decorators_ws.py
@@ -10,14 +10,28 @@ def logger(path):
         def new_function(*args, **kwargs):
             result = old_function(*args, **kwargs)
             with open(path, 'a', encoding='utf-8') as f_log:
-                f_log.write(
-                    f'\ncall time: {datetime.datetime.now()}; function name: {new_function.__name__}; with arguments: {args} and {kwargs}; returned: {result}')
+                f_log.write(f'\ncall time: {datetime.datetime.now()}; function name: {new_function.__name__}; with arguments: {args} and {kwargs}; returned: {result}')
 
             return result
 
         return new_function
 
     return __logger
+
+
+def log_clean(path):
+    def __log_clean(old_function):
+        @wraps(old_function)
+        def new_function(*args, **kwargs):
+            f = open(path, 'w')
+            f.write(f'\ncall time: {datetime.datetime.now()}; function name: {new_function.__name__}')
+            f.close()
+            result = old_function(*args, **kwargs)
+            return result
+
+        return new_function
+
+    return __log_clean
 
 
 def with_attempts(max_attempts=3, timeout=0.1):
@@ -27,7 +41,7 @@ def with_attempts(max_attempts=3, timeout=0.1):
             for i in range(1, max_attempts + 1):
                 result = any_function(*args, **kwargs)
                 if result[1] == 200:
-                    print(f'Успешное подключение к {result[2]}')
+                    # print(f'Успешное подключение к {result[2]}')
                     break
                 else:
                     print(f'Не удалось подключиться к {result[2]}')
@@ -37,3 +51,22 @@ def with_attempts(max_attempts=3, timeout=0.1):
         return new_function
 
     return _with_attempts
+
+
+def with_attempts_data(max_attempts=3, timeout=0.1):
+    def _with_attempts_data(any_function):
+        @wraps(any_function)
+        def new_function(*args, **kwargs):
+            for i in range(1, max_attempts + 1):
+                result = any_function(*args, **kwargs)
+                if result[0] is not None:
+                    print(f'Получено описание вакансии со страницы: {result[1]}')
+                    break
+                else:
+                    print(f'Не получены данные со страницы: {result[1]}')
+                    time.sleep(timeout)
+            return result
+
+        return new_function
+
+    return _with_attempts_data
--- application/hh_srap.py
+++ application/hh_srap.py
@@ -7,7 +7,8 @@ import json
 from functools import wraps
 import requests
 from bs4 import BeautifulSoup
-from application.decorators_ws import logger, with_attempts
+from application.decorators_ws import logger, with_attempts, with_attempts_data, log_clean
+
 
 class HhScrap:
 
@@ -24,24 +25,35 @@ class HhScrap:
         response = requests.get(url=url, params=params, headers=self.headers)
         return response, response.status_code, url
 
+    @with_attempts_data(max_attempts=5, timeout=0.5)
     def get_html(self, page):
         params = {
             'items_on_page': self.item,
             'page': page
         }
-        # response = requests.get(url=self.url, params=params, headers=self.headers)
         response = self.requests_conn(self.url, params=params)[0]
         html_data = response.text
         hh_filter = BeautifulSoup(html_data, 'lxml')
         vacancy_list_tag = hh_filter.find('div', id="a11y-main-content")
-        vacancy_tags = vacancy_list_tag.find_all('div', class_="serp-item")
-        return vacancy_list_tag, vacancy_tags
+        if vacancy_list_tag is not None:
+            vacancy_tags = vacancy_list_tag.find_all('div', class_="serp-item")
+        else:
+            print(f'Данные страницы не получены')
+            vacancy_tags = None
+        return vacancy_tags, f'Страница с перечнем вакансий № {page + 1}'
+
+    @with_attempts_data(max_attempts=5, timeout=0.5)
+    def get_vacancy_description(self, url):
+        description_page = self.requests_conn(url)[0]
+        description = BeautifulSoup(description_page.text, 'lxml')
+        description_body_tag = description.find('div', class_="vacancy-section")
+        return description_body_tag, url
 
     def scrap_pages(self, vacancy_tags):
         vacancy_parsed = {}
         all_get = []
         for vacancy_tag in vacancy_tags:
-            time.sleep(0.33)
+            time.sleep(0.1)
             header_tag = vacancy_tag.find('h3')
             a_tag = header_tag.find('a')
             company_tag = vacancy_tag.find('div', class_="vacancy-serp-item__meta-info-company")
@@ -54,11 +66,15 @@ class HhScrap:
             city_text = city_tag.text.split()[0].strip(',')
             id_vacancy = re.search('\d+', link).group()
 
-            # description_page = requests.get(link, headers=self.headers)
-            description_page = self.requests_conn(link)[0]
-            description = BeautifulSoup(description_page.text, 'lxml')
-            description_body_tag = description.find('div', class_="vacancy-section")
-            description_body_text = description_body_tag.text
+            # description_page = self.requests_conn(link)[0]
+            # description = BeautifulSoup(description_page.text, 'lxml')
+            # description_body_tag = description.find('div', class_="vacancy-section")
+            description_body_tag = self.get_vacancy_description(link)[0]
+            if description_body_tag is not None:
+                description_body_text = description_body_tag.text
+            else:
+                print(f'Страница {link} пропущена т.к. данные не получены')
+                continue
 
             if salary_tag is not None:
                 salary_text = unicodedata.normalize("NFKD", salary_tag.text)
@@ -94,25 +110,28 @@ class HhScrap:
         total_get = []
         page = 0
         while len(all_vacancy_parsed) < self.how_many_search:
-            time.sleep(0.5)
-            vacancy_list_tag, vacancy_tags = self.get_html(page)
-            if vacancy_list_tag is not None:
+            time.sleep(0.1)
+            vacancy_tags = self.get_html(page)[0]
+            if vacancy_tags is not None:
                 page += 1
             else:
+                print()
                 print('Собраны все существующие вакансии')
                 break
             vacancy_parsed, all_get = self.scrap_pages(vacancy_tags)
             all_vacancy_parsed.update(vacancy_parsed)
             total_get.extend(all_get)
             print(f'страница {page}')
-            print(
-                f'В цикле обработано {len(all_get)} вакансий. Всего подошло по условиям вакансий {len(all_vacancy_parsed)}')
+            print(f'В цикле обработано {len(all_get)} вакансий. Всего подошло по условиям вакансий {len(all_vacancy_parsed)}')
+        print()
         print('Сбор данных завершен')
-        print(
-            f'всего обработано {len(total_get)} вакансий из них подошло по условиям {len(all_vacancy_parsed)} вакансий')
-        return all_vacancy_parsed
+        print(f'всего обработано {len(total_get)} вакансий из них подошло по условиям {len(all_vacancy_parsed)} вакансий')
+        return all_vacancy_parsed, total_get
 
+    @log_clean(path='data/requests.log')
     def create_json(self):
         with open('data/vacancy_parsed.json', 'w', encoding='utf-8') as file:
-            json.dump(self.get_all_vacancy(), file, indent=4, ensure_ascii=False)
+            dict_vacancy, total_get = self.get_all_vacancy()
+            json.dump(dict_vacancy, file, indent=4, ensure_ascii=False)
         print('Файл "vacancy_parsed.json" успешно создан')
+        return total_get
